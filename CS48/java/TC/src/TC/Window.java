63a1378e8af4d25de11052a332e5178586c83395
Tamky Ngo <t_ngo@umail.ucsb.edu>
2015-01-27 00:00:04 -0800

    here are all the files for our project that Stone provided

---
+++ CS48/java/TC/src/TC/Window.java
@@ -0,0 +1,27 @@
+package TC;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Window extends JFrame{
+	public Window(){
+		this.setTitle("Tank Command"); 
+		this.setSize(1000, 530);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false); 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		//create framework that extends the canvas and put it on the frame 
+		this.setContentPane(new Framework());
+		this.setVisible(true); 
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				new Window(); 
+			}}); 
+		
+	}
+
+}
