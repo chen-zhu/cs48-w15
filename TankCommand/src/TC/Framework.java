dbba37f5110e578ed8338754d77d36f2410f7ec8
Stone (Chen) Zhu <chen_zhu@umail.ucsb.edu>
2015-03-06 10:35:04 -0800

    change sound.

--- TankCommand/src/TC/Framework.java
+++ TankCommand/src/TC/Framework.java
@@ -58,8 +58,12 @@ public class Framework extends drawingpanel{
 	private BufferedImage cloud; 
 	private BufferedImage black; 	
 	
+	//number control 
+	public int count=0; 
+	
 	//background music 
 	static AudioClip clip; 
+	public URL music; 
 	
 	//update, showing the position of mouse
 	private Point mouseposition(){
@@ -329,9 +333,32 @@ public class Framework extends drawingpanel{
 				newgame(); 
 			}
 		});
-		c=new JButton("     High Score     "); 
+		
+		
+		//change the background music 
+		c=new JButton("   Sound Change   "); 
 		c.setFocusable(false);
 		add(c);  
+		c.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				count++; 
+				if(count % 3 == 0){
+					music=this.getClass().getResource("/TC/resources/sound/firework1.wav");
+					clip = Applet.newAudioClip(music);
+					c.setText("   \"Firework\"   ");
+				}
+				else if(count%3 == 1){
+					music=this.getClass().getResource("/TC/resources/sound/firework.wav");
+					clip = Applet.newAudioClip(music);
+					c.setText("   \"Desperate\"   ");
+				}
+				else if(count%3 == 2){
+					music=this.getClass().getResource("/TC/resources/sound/Space.wav");
+					clip = Applet.newAudioClip(music);
+					c.setText("  \"Blank Space\"  ");	
+				}
+			}
+		}); 
 		
 		
 		d=new JButton("   Music On   "); 
